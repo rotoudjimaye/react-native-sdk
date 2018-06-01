2434e21c06636373822faaeffdc5830774d6a94e
Patrick Buitre <patrickbuitre@Pats.local>
2018-06-01 19:24:10 +0800

    Made compatible with react-native < 0.47

--- android/app/src/main/java/com/lenddo/data/RNDataSdkWrapperPackage.java
+++ android/app/src/main/java/com/lenddo/data/RNDataSdkWrapperPackage.java
@@ -2,15 +2,21 @@ package com.lenddo.data;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import com.lenddo.mobile.core.Log;
+import java.util.Collections;
 
 public class RNDataSdkWrapperPackage implements ReactPackage {
     private static final String TAG = "RNDataSdkWrapperPackage";
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+       return Collections.emptyList();
+    }
 
     @Override
     public List<NativeModule> createNativeModules(
